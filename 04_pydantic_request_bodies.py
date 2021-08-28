8475ed917cb23f0ef9a8b75c1af179a973ad710b
symonk <jackofspaces@gmail.com>
2021-08-28 12:20:07 +0100

    using pydantic for request bodies

--- 04_pydantic_request_bodies.py
+++ 04_pydantic_request_bodies.py
@@ -3,4 +3,29 @@ Example of receiving a client request body
 """
 
 from _util import app
+from pydantic import BaseModel
 
+
+class Car(BaseModel):
+    wheels: int = 4
+    color: str = "Red"
+    top_speed: float = 144.00
+
+
+@app.post("/cars")
+async def create_car(car: Car):
+    return car
+
+
+@app.get("/cars")
+async def get_all_cars():
+    return [Car()]
+
+
+"""
+(.venv)  si@ubuntu  ~/workspaces/introduction-to-fastapi   main ±  curl -X POST -H "Content-Type: application/json" -d '{"wheels": 3}' localhost:8000/cars     
+{"wheels":3,"color":"Red","top_speed":144.0}
+
+(.venv)  si@ubuntu  ~/workspaces/introduction-to-fastapi   main ±  curl localhost:8000/cars                                                                    
+[{"wheels":4,"color":"Red","top_speed":144.0}]
+"""
